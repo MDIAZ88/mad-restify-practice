ae04a0ef875a4dd781cdaf6a95b9b0449c2a12a7
MDIAZ88 <marcos.diaz@globant.com>
2015-08-24 17:49:49 -0300

    restify two

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,17 @@
 
+module.exports = {
+  '/': {
+    get: function(req, res, cb) {
+      res.json({});
+      return cb();
+    }
+  },
+  '/test': {
+    get: function(req, res, cb) {
+      setTimeout(function() {
+        res.send('GO AWAY');
+        res.end();
+      }, 5000);
+    }
+  }
+};
